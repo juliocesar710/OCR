ff699b8a8b2942877b0df95890a9c17a727a6d33
julio candeia <juliocandeia594@gmail.com>
2025-03-21 13:48:42 -0300

    feat: up url conect backend

--- next/src/api/api.ts
+++ next/src/api/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'http://localhost:3001',
+  baseURL: 'https://ocr-n7wk.onrender.com',
   headers: {
     'Content-Type': 'application/json',
   },
